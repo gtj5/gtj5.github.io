7917cb68df15f055e7b074623fa7c5507c6f70d1
gtj5 <jean.lunacy@outlook.com>
2023-12-28 16:33:20 -0400

    nice

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const header = document.querySelector('.navbar');
 
 window.onscroll = function(){
     var top = window.scrollY;
-    if (top >= 100){
+    if (top >= 150){
         header.classList.add('navbarDark');
     }
     else{
